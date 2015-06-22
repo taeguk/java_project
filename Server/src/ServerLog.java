af673ea57ab6c53807b7f6766ef800aa18b65769
taeguk <xornrbboy@gmail.com>
2015-06-22 21:47:01 +0900

    delete olds

--- Server/src/ServerLog.java
+++
@@ -1,9 +0,0 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class ServerLog {
-	static String getTime() {
-		SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
-		return f.format(new Date());
-	}
-}
